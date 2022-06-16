cfb22c662bc7282bf53e0ab9eedbadd5e8dae5ae
Michaela-K <michaelakodes@gmail.com>
2022-06-16 14:18:51 +0000

    bugFix - Money Formatting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,8 @@ class CategoriesController < ApplicationController
     @products = @category.products.order(created_at: :desc)
   end
 
+  # def humanized_money_with_symbol(value, options={})
+  #   humanized_money(value, options.merge(:symbol => true))
+  # end
+
 end
