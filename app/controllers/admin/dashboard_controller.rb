0fa27112ec2229f5b152c64d3aeba9c09fdf19dc
Michaela-K <michaelakodes@gmail.com>
2022-06-18 02:16:29 +0000

    create and style stats table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name:ENV['BA_NAME'], password:ENV['BA_PASS']
   def show
+    @products = Product.count
+    @category = Category.count
   end
+
 end
