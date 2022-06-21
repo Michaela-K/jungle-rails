3d2a6e54abf580a3d5504102a9755722e83e51dc
Michaela-K <michaelakodes@gmail.com>
2022-06-21 15:22:28 +0000

    display active sale on every page

--- app/helpers/admin/sales_helper.rb
+++ app/helpers/admin/sales_helper.rb
@@ -1,2 +1,9 @@
 module Admin::SalesHelper
+
+  def active_sale?
+    # ? are place holders, they represents todays date
+    #inserting placeholders means you need to now provide dynamic values for it to work
+    Sale.where("sales.starts_on <= ? AND sales.ends_on >= ?",
+     Date.current, Date.current).any?
+  end
 end
