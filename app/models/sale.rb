7aeb457c73e0edcd0c77d96484769bb73642cfae
Michaela-K <michaelakodes@gmail.com>
2022-06-21 14:42:19 +0000

    DRY up the status on sales view

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,13 @@
 class Sale < ApplicationRecord
 
-  
+  def finished?
+    ends_on < Date.current
+  end
+  def finished?
+    starts_on > Date.current
+  end
+  def active?
+    !upcoming && !finished
+  end
 
 end
