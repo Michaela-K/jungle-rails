4a9c80b08a3a3a40f8df25c26414784d57e773bc
Michaela-K <michaelakodes@gmail.com>
2022-07-01 14:41:17 +0000

    refactor user spec and fix bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true,
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
             format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
-                      message: 'The format of Email is invalid'}
+            message: 'The format of Email is invalid'}
   validates :password, presence: true, :length => {:minimum => 8 }
+  validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,40 +9,48 @@ RSpec.describe User, type: :model do
       expect(@user).to have_attributes(:firstname => "Rose", :lastname => "Nylund", :email => "rosenylund@gmail.com", :password => 'password',:password_confirmation =>'password')
     end
 
+    #password and password_confirmation required when making a model
+    it "ensure password and password_confirmation required" do
+     @user = User.new(email: "rosenylund@gmail.com", password: 'password',password_confirmation:nil )
+     expect(@user.valid?).to be_falsey
+     expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+
     #validation if :password and password_confirmation fields dont match
-    it "ensure password and password_confirmation fields dont match" do
+    it "ensure password and password_confirmation fields match" do
       @user = User.new(password: 'password',password_confirmation:'password' )
       expect(@user.password).to eql(@user.password_confirmation)
     end
 
     #emails must be unique(not case sensitive) therefore no duplicates
     it "ensures emails are not duplicated" do
-      @user = User.new(email: "rosenylund@gmail.com")
+      @user = User.new(email: "rosenylund@gmail.com", password: 'password',password_confirmation:'password')
+      @user.save
+
       @invalid_user = @user.dup   # dup is a “shallow” copy as it copies the object's attributes only
-      @invalid_user.email = "ROSENYLUND@GMAIL.COM"
-      
-      #!!!@invalid_user.valid? returns falsey in the terminal -> how does it know? - because it only copied the attributes and nothing more like id, timestamps etc?
-      expect(@invalid_user.valid?).to be_falsey  #valid? -> Returns true if no errors are found, false otherwise.
-      expect(@invalid_user.errors.full_messages[0]).to match("Password can't be blank")                                                                          
+      @invalid_user.email.upcase!
+      @invalid_user.save
+  
+      expect(@invalid_user).not_to be_valid  #valid? -> Returns true if no errors are found, false otherwise.
+      expect(@invalid_user.errors.full_messages).to include("Email has already been taken")                                                                          
     end
 
     #password min length when account being created 
     it "validates password length" do
       @user = User.new(password: "rose")
       expect(@user.valid?).to be_falsey
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 8 characters)")
     end
-
-    #password and password_confirmation required when making a model
   end
 
 
   describe '.authenticate_with_credentials' do
-    # it 'should validate emails with spaces before/after' do
-    #   @user = User.new(:email => '  rose@gmail.com  ')
-    #   @user[:email].strip!
+    it 'should validate emails with spaces before/after' do
+      @user = User.new(:email => '  rose@gmail.com  ')
+      @user[:email].strip!
 
-    #   expect(@user).to have_attributes(:email => 'rose@gmail.com')
-    # end
+      expect(@user).to have_attributes(:email => 'rose@gmail.com')
+    end
 
     # it "should validate when email is in the wrong case" do
     #   @user = User.new(:email => 'rOse@gmail.cOm', :password => 'password')
