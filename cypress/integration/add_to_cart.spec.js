4028d416c740c6e110fea96941dbc779f9b381b7
Michaela-K <michaelakodes@gmail.com>
2022-07-24 13:05:32 +0000

    fix bug in cypress  - add to cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,25 +1,12 @@
-describe("test that users can click the 'Add to Cart' button for a product ", () => {
+describe("test that users can click the 'Add to Cart' button for a product on the home page", () => {
   beforeEach(() => {
     cy.visit('http://0.0.0.0:3000')
   })
   
   //test that it can click add to cart
-  it("Can click add to cart", () => {
-    // cy.get(".btn").first().should('contain', 'Add').click({ force: true });
-    // cy.get('.products article div form button').first().click({ force: true });
-    // cy.get('a').should('have.attr', 'href', '/cart').click().get('.products article div form button').first().click();
-    cy.get('ul>li').each(($el) => {
-      if ($el == cy.get('.nav-link').should('have.attr', 'href', '/cart')){
-        cy.click($el).get('.products article div form button').click();
-      }
-    })
-    // cy.get('.btn').find('[type="submit"]').first().contains("Add").click();
-  });
-  
-  //cart increases by one
-  it("Cart should increase by one", () => {
-    cy.get(".nav-link").should('contain', 'My Cart (0)');
-    // cy.get('.btn').find('[type="submit"]').first().contains("Add").click();
-  });
+  it("Can click add to cart and the cart should increase by one", () => {
+    cy.get('.container .products article div form').find('.btn').first().debug().click({force: true});
+    cy.get(".end-0 .nav-link").should('contain', 'My Cart (1)');
 
+  });
 });
